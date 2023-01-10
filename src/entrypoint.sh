9133ea403879a6d0723fc608dc97ebb3b9b6374b
Will Thames <wthames@skedulo.com>
2023-01-10 14:09:12 +1000

    Explicitly set kustomize url
    Seems like a waste to try and obtain the URL when it can be
    constructed

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -48,28 +48,10 @@ function parse_inputs {
 
 function install_kustomize {
 
-    echo "getting download url for kustomize ${kustomize_version}"
-
-    for i in {1..100}; do
-      url=$(curl --retry-all-errors --fail --retry 30 --retry-max-time 120 "${with_token[@]}" -s "https://api.github.com/repos/kubernetes-sigs/kustomize/releases?per_page=100&page=$i" | jq -r '.[].assets[] | select(.browser_download_url | test("kustomize(_|.)?(v)?'$kustomize_version'_linux_amd64"))  | .browser_download_url')
-      if [ ! -z $url ]; then
-        break
-      fi
-    done
-
-    if [ ! -z $url ]; then
-      echo "Download URL found in $url"
-    else
-      echo "Failed to find download URL for ${kustomize_version}"
-      exit 1
-    fi
+    url="https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${kustomize_version}/kustomize_v${kustomize_version}_linux_amd64.tar.gz"
 
     echo "Downloading kustomize v${kustomize_version}"
-    if [[ "${url}" =~ .tar.gz$ ]]; then
-      curl --retry 30 --retry-max-time 120 -s -S -L ${url} | tar -xz -C /usr/bin
-    else
-      curl --retry 30 --retry-max-time 120 -s -S -L ${url} -o /usr/bin/kustomize
-    fi
+    curl --retry 30 --retry-max-time 120 -s -S -L ${url} | tar -xz -C /usr/bin
     if [ "${?}" -ne 0 ]; then
         echo "Failed to download kustomize v${kustomize_version}."
         exit 1
